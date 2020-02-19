5a56a27892af5fc323d84ed83138b5a5f33e1d01
N. Uzoma Ariguzo <u.ariguzo.ua@gmail.com>
2020-02-19 17:11:41 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index { |item, index|
+    puts "#{index + 1}. #{item}"
+  }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  new_array = Array.new
+  array.map {|n| 
+    new_array << "#{n.capitalize}!"
+    }
+  new_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  index = 0
+  while index < array.length do
+    if array[index].length === 4
+      return false
+    else
+      return true
+    end
+    index += 1
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snack)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snack.find { |a| cheese_types.include?(a) }
 end
